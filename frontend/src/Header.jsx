83f6d1a167d99e448265bccf98c6c46f18e9f0c8
darman <dxlacerate@gmail.com>
2023-08-15 11:07:35 +0700

    adding the header sign in

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -1,29 +1,43 @@
-import React from 'react';
-import { Button, Input, InputGroup, InputRightElement, Image, Box, Flex } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
-import logo from '../src/assets/logo.png'; // Import the logo image
+import React, { useState } from 'react';
+import { Flex, Button } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom'; // Import RouterLink from react-router-dom
+import logo from '../src/assets/logo.png'; // Make sure logo.png is located in the src directory
+import SearchBar from '../src/SearchBar';
+import SearchResults from '../src/SearchResults';
 
 const Header = () => {
+    const [searchResults, setSearchResults] = useState([]);
+
+    const handleSearch = (searchTerm) => {
+        // Simulate fetching search results from an API
+        // Replace this with your actual search logic
+        const dummySearchResults = [
+            'Search Result 1',
+            'Search Result 2',
+            'Search Result 3',
+        ];
+
+        // Update search results based on the searchTerm
+        const filteredResults = dummySearchResults.filter(result =>
+            result.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setSearchResults(filteredResults);
+    };
+
     return (
-        <header style={{ height: '80px' }}> {/* Adjust header height */}
-            <Box p={4}>
-                <Flex align="center" justify="space-between">
-                    <Link to="/">
-                        <Image src={logo} alt="Logo" w="150px" /> {/* Display the logo image */}
-                    </Link>
-                    <InputGroup maxW="md">
-                        <Input placeholder="Search..." />
-                        <InputRightElement>
-                            <Button colorScheme="brandGreen" size="sm">
-                                Search
-                            </Button>
-                        </InputRightElement>
-                    </InputGroup>
-                    <Button as={Link} to="/login" colorScheme="teal" size="sm">
-                        Sign In
-                    </Button>
-                </Flex>
-            </Box>
+        <header>
+            <Flex align="center" justify="space-between" p={4}>
+                {/* Make the logo a clickable link */}
+                <RouterLink to="/">
+                    <img src={logo} alt="Logo" style={{ width: '150px', cursor: 'pointer' }} />
+                </RouterLink>
+                <SearchBar onSearch={handleSearch} />
+                {/* Use RouterLink to navigate to the login page */}
+                <Button as={RouterLink} to="/login" colorScheme="teal" size="sm">
+                    Sign In
+                </Button>
+            </Flex>
+            <SearchResults results={searchResults} />
         </header>
     );
 };
--- frontend/src/HomePage.jsx
+++ frontend/src/HomePage.jsx
@@ -3,10 +3,15 @@ import { SimpleGrid, Image } from '@chakra-ui/react';
 
 const HomePage = () => {
     return (
-        <SimpleGrid columns={3} spacing={4} p={8}>
-            <Image src="https://via.placeholder.com/150" alt="Thumbnail 1" borderRadius="md" />
-            <Image src="https://via.placeholder.com/150" alt="Thumbnail 2" borderRadius="md" />
-            <Image src="https://via.placeholder.com/150" alt="Thumbnail 3" borderRadius="md" />
+        <SimpleGrid columns={7} spacing={4} p={8}>
+            <Image src="https://via.placeholder.com/360x640" alt="Thumbnail 1" borderRadius="md" />
+            <Image src="https://via.placeholder.com/360x640" alt="Thumbnail 2" borderRadius="md" />
+            <Image src="https://via.placeholder.com/360x640" alt="Thumbnail 3" borderRadius="md" />
+            <Image src="https://via.placeholder.com/360x640" alt="Thumbnail 3" borderRadius="md" />
+            <Image src="https://via.placeholder.com/360x640" alt="Thumbnail 3" borderRadius="md" />
+            <Image src="https://via.placeholder.com/360x640" alt="Thumbnail 3" borderRadius="md" />
+            <Image src="https://via.placeholder.com/360x640" alt="Thumbnail 3" borderRadius="md" />
+
         </SimpleGrid>
     );
 };
